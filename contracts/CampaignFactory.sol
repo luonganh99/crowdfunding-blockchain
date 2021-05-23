2f1a118b3294028e0458cdafb8544d8a7991eca9
thaison247 <nguyenthaison247@gmail.com>
2021-05-23 10:42:09 +0700

    Added CampaignFactory.sol file

---
+++ contracts/CampaignFactory.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+import "./Campaign.sol";
+
+contract CampaignFactory {
+    address[] public campaigns;
+
+    function createCampaign(uint256 minimum) public {
+        Campaign campaign = new Campaign(minimum, msg.sender);
+        campaigns.push(address(campaign));
+    }
+
+    function getCampaigns() public view returns (address[]) {
+        return campaigns;
+    }
+}
